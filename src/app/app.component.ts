586fa548d82da3da0c38c7a7950d4546984b7cd0
20Stacks G Code <20StacksGcode@Gs-MacBook-Pro.local>
2020-06-21 12:37:01 -0700

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ERP';
+  myName = "Gareth Butler";
+
+  constructor(){
+    console.log("hello, I'm the constructor");
+
+    var name: string = "Gareth Butler";
+    var age: number = 99;
+    var found: boolean = false;
+    var list: string[] = [];
+
+    var future = 0;
+
+    this.hello(this.title);
+    this.hello(9);
+  }
+
+  hello(name){
+    console.log(name);
+  }
+
+  something(){
+    console.log("Btn clicked!");
+    this.title="This is awesome!!";
+  }
+
 }
---
+++ src/app/components/converter/converter.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.scss']
+})
+export class ConverterComponent implements OnInit {
+
+  farenheit = 0.0;
+  celcius = 0.0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  convertToC(){
+    // (100˚F - 32) x 5/9 = 37.778˚C
+    this.celcius = (this.farenheit - 32) * 5/9;
+  }
+
+  convertToF(){
+    //(40˚C x 9/5) + 32 = 104˚F
+    this.farenheit = (this.celcius * 9/5) + 32;
+    }
+
+}
